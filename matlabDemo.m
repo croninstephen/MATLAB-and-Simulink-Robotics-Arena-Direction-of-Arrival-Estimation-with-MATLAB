c1129c2355ceeabecea63d7e049a8b981a991402
croninstephen <cronins1@my.erau.edu>
2017-08-28 12:17:55 -0400

    Bug Fix
    Fixed an error with mat file conversion

--- matlabDemo.m
+++ matlabDemo.m
@@ -8,8 +8,8 @@ load('pulseData.mat');
 
 %%
 % Bandpass
-bandPassLow  = 39.99*1000;
-bandPassHigh = 40.01*1000;
+bandPassLow  = 39.98*1000;
+bandPassHigh = 40.02*1000;
 frequency = 40000;
 Fs = 500000;
 
@@ -25,10 +25,11 @@ data1(5,:) = filter(bpFiltIir,data(5,:));
 
 %%
 % Graphing
-subplot(1,3,1);
+%subplot(1,3,1);
+figure(1);
 hold on;
-lower = 1;
-upper = 250001;
+lower = 500000;
+upper = 750000;
 plot(data(1, lower:upper), data(2, lower:upper));
 plot(data(1, lower:upper), data(3, lower:upper));
 plot(data(1, lower:upper), data(4, lower:upper));
@@ -36,10 +37,11 @@ plot(data(1, lower:upper), data(5, lower:upper));
 hold off;
 
 
-subplot(1,3,2);
+% subplot(1,3,2);
+figure(2);
 hold on;
-lower = 1;
-upper = 250001;
+lower = 500000;
+upper = 750000;
 plot(data1(1, lower:upper), data1(2, lower:upper));
 plot(data1(1, lower:upper), data1(3, lower:upper));
 plot(data1(1, lower:upper), data1(4, lower:upper));
@@ -47,11 +49,12 @@ plot(data1(1, lower:upper), data1(5, lower:upper));
 hold off;
 
 
-threshold = 0.0001;
+threshold = 0.00015;
 length = 200;
 f = find(data1(2, (lower):upper) > threshold, 1);
 
-subplot(1,3,3);
+% subplot(1,3,3);
+figure(3);
 hold on;
 plot(data1(1, f:f+length), data1(2, f:f+length));
 plot(data1(1, f:f+length), data1(3, f:f+length));
